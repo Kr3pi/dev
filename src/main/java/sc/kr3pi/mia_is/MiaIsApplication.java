9d13b69e17f6f38eb4d99cde23df637d7529c78c
kr3pi <hencel.hollanda@unisey.ac.sc>
2022-06-19 19:20:34 +0400

    init

---
+++ src/main/java/sc/kr3pi/mia_is/MiaIsApplication.java
@@ -0,0 +1,13 @@
+package sc.kr3pi.mia_is;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sc/kr3pi/mia_is/MiaIsApplicationTests.java
@@ -0,0 +1,13 @@
+package sc.kr3pi.mia_is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
